d9cf4fd8864368139eb7f35a54f0cb69f594b928
Cerem Cem ASLAN <cem@aktos.io>
2017-06-12 02:49:35 +0300

    renamed tool

--- sync-local.sh
+++ sync-local.sh
@@ -3,14 +3,60 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 . $DIR/common.sh
 
-require_mounted $SRC1
-require_mounted $SRC2
-require_mounted $DEST
+for i in $SRC1 $SRC2 $DEST; do
+    require_mounted $i
+done
 
-require_being_btrfs_subvolume $DEST_SNAP 
+require_being_btrfs_subvolume $DEST_SNAP
 
-echo "Syncing $SRC1_SNAP"
-buttersink $SRC1_SNAP/ $DEST_SNAP || echo_err "error in syncing $SRC1_SNAP"
 
-echo "Syncing $SRC2_SNAP"
-buttersink $SRC2_SNAP/ $DEST_SNAP || echo_err "error in syncing $SRC2_SNAP"
+#SOURCE_SNAPS="$SRC1_SNAP/$SRC1_SUB1"
+#DESTINATION_SNAPS="$DEST_SNAP/$SRC1_SUB1"
+SOURCE_SNAPS="$SRC2_SNAP/$SRC2_SUB1"
+DESTINATION_SNAPS="$DEST_SNAP/$SRC2_SUB1"
+echo "Sendings snapshots from $SOURCE_SNAPS to $DESTINATION_SNAPS"
+echo "===================================================================="
+
+ref_snapshot=""
+while read -a src; do
+    if [[ "$(get_snapshot_in_dest $src $DESTINATION_SNAPS)" != "" ]]; then
+        echo "$src exists in destination, no need to send again."
+        ref_snapshot=$src
+    else
+        echo "$src will be sent to destination."
+        # Minimum difference algorithm:
+        # --------------------------------
+        # since this utility is designed to send incremental backups, most
+        # probably the minimum amount of difference will be optained between the
+        # previous snapshot which has been successfully sent to the destination
+        # and this one.
+
+        dest_snap_path="$DESTINATION_SNAPS/$(basename $src)"
+        echo "trying to rename to backup the $dest_snap_path in destination."
+        mv $dest_snap_path "$dest_snap_path.backup-$(get_timestamp)" 2> /dev/null
+
+        # over file
+        if false; then
+            tmp_file="/home/ceremcem/tmp/$(basename $src)"
+            btrfs send -v -p $ref_snapshot -f $tmp_file $src || echo_err "Problem in btrfs send"
+            btrfs receive -f $tmp_file $DESTINATION_SNAPS
+            echo "DO NOT FORGET TO REMOVE $tmp_file"
+        else
+            # directly
+            # simulating send (in order to show the total stream size)
+            ####btrfs send -v -p $ref_snapshot $src | pv > /dev/null
+            # actual transfer
+            btrfs send -v -p $ref_snapshot $src | pv | btrfs receive $DESTINATION_SNAPS
+        fi
+    fi
+
+    echo "--------------------------------------------------------------------"
+    continue
+    if is_subvolume_successfully_sent $dest; then
+        echo "subvol ok"
+    else
+        echo "subvol will be deleted"
+        #btrfs sub delete $snap
+    fi
+
+done < <(snapshots_in $SOURCE_SNAPS)
