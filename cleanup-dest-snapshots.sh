0ca6993791a7b871ec128ac8578cac58e0e3ce94
Cerem Cem ASLAN <cem@aktos.io>
2017-06-12 16:20:22 +0300

    added timers

--- cleanup-dest-snapshots.sh
+++ cleanup-dest-snapshots.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 . $DIR/common.sh
+start_timer
 
 needed_space="200G"
 echo_green "Starting cleanup to make $needed_space of free space in $(mount_point_of $DEST_SNAP)"
@@ -9,3 +10,4 @@ cleanup_dest_snapshots_by_disk_space $needed_space "$SRC2_SNAP/$SRC2_SUB1" "$DES
 cleanup_dest_snapshots_by_disk_space $needed_space "$SRC1_SNAP/$SRC1_SUB1" "$DEST_SNAP/$SRC1_SUB1"
 
 echo_green "Cleanup done, current free space: $(get_free_space_of_snap $DEST_SNAP) K"
+show_timer
--- cleanup-src-snapshots.sh
+++ cleanup-src-snapshots.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 . $DIR/common.sh
+start_timer
 
 echo_green "Cleaning up $(mount_point_of $SRC2_SNAP)"
 cleanup_src_snapshots_by_disk_space "200G" "$SRC2_SNAP/$SRC2_SUB1" "$DEST_SNAP/$SRC2_SUB1"
@@ -9,3 +10,4 @@ echo_green "Cleaning up $(mount_point_of $SRC1_SNAP)"
 cleanup_src_snapshots_by_disk_space "5G" "$SRC1_SNAP/$SRC1_SUB1" "$DEST_SNAP/$SRC1_SUB1"
 
 echo_green "Cleanup done"
+show_timer
