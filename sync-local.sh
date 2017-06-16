0cace7e41df41076e862620960c74af70d2c0793
Cerem Cem ASLAN <cem@aktos.io>
2017-06-16 11:27:45 +0300

    rootfs is synchronized first

--- sync-local.sh
+++ sync-local.sh
@@ -12,8 +12,8 @@ require_being_btrfs_subvolume $DEST_SNAP
 
 echo_green "Started sync-local"
 
-send_all_snapshots "$SRC2_SNAP/$SRC2_SUB1" "$DEST_SNAP/$SRC2_SUB1"
-show_timer "sync of $SRC2_SNAP completed"
-
 send_all_snapshots "$SRC1_SNAP/$SRC1_SUB1" "$DEST_SNAP/$SRC1_SUB1"
+show_timer "sync of $SRC1_SNAP completed"
+
+send_all_snapshots "$SRC2_SNAP/$SRC2_SUB1" "$DEST_SNAP/$SRC2_SUB1"
 show_timer "Synchronization finished."
