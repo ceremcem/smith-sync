00c29fb3b690da0d3922b77ddda8ee8342cd40c8
Cerem Cem ASLAN <cem@aktos.io>
2017-06-18 01:01:36 +0300

    taking correct rollback snapshot

--- take-snapshot.sh
+++ take-snapshot.sh
@@ -8,18 +8,17 @@ start_timer
 # to delete a mounted snapshot
 require_not_mounted $ROLLBACK_SNAPSHOT
 
-new_rollback_snapshot=$(last_snapshot_in "$SRC1_SNAP/$SRC1_SUB1")
+echo_green "Snapshotting current rootfs as rollback ($ROLLBACK_SNAPSHOT)"
+mv $ROLLBACK_SNAPSHOT $ROLLBACK_SNAPSHOT.del
+btrfs sub snap / $ROLLBACK_SNAPSHOT
+btrfs sub delete $ROLLBACK_SNAPSHOT.del
+show_timer "Current rootfs is now in rollback location."
 
-echo "Taking Snapshots..."
-TIMESTAMP=$(get_timestamp)
-take_snapshot "$SRC1/$SRC1_SUB1" "$SRC1_SNAP/$SRC1_SUB1/$SRC1_SUB1.$TIMESTAMP"
-take_snapshot "$SRC2/$SRC2_SUB1" "$SRC2_SNAP/$SRC2_SUB1/$SRC2_SUB1.$TIMESTAMP"
 
+echo_green "Taking Snapshots..."
+POSTFIX=$(get_timestamp)
+take_snapshot "$SRC1/$SRC1_SUB1" "$SRC1_SNAP/$SRC1_SUB1/$SRC1_SUB1.$POSTFIX"
+take_snapshot "$SRC2/$SRC2_SUB1" "$SRC2_SNAP/$SRC2_SUB1/$SRC2_SUB1.$POSTFIX"
 show_timer "All snapshots are taken."
 
-echo "Putting last snapshot ($new_rollback_snapshot) in rollback location ($ROLLBACK_SNAPSHOT)"
-mv $ROLLBACK_SNAPSHOT $ROLLBACK_SNAPSHOT.del
-btrfs sub snap $new_rollback_snapshot $ROLLBACK_SNAPSHOT
-btrfs sub delete $ROLLBACK_SNAPSHOT.del
-
-show_timer "Taking snapshots process ended. "
+show_timer "All done."
