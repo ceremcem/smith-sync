f05050ede5b5be12c43189424a35f01a2c9eef8a
Cerem Cem ASLAN <cem@aktos.io>
2017-07-17 23:22:55 +0300

    switched to aktos-dcs-lib. fixed detach-disk.sh

--- erik-tools/detach-disk.sh
+++ erik-tools/detach-disk.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 #
-# pass "--force" as first argument if you want to remove disks forcibly.
+# pass "-f" as first argument if you want to remove disks forcibly.
 #
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-. $DIR/common.sh
+set_dir () { DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"; }; set_dir
+safe_source () { source $1; set_dir; }
+
+
+safe_source $DIR/config.sh
+safe_source $DIR/../common.sh
 
 FORCE_FLAG=$1
 
-umount_if_mounted $FORCE_FLAG $ROOT_PART
+umount_if_mounted2 $ROOT_PART $FORCE_FLAG
 remove_lvm_parts || echo_err "Problem while removing lvm parts."
 remove_crypted_part
